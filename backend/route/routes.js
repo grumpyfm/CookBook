3e6121386d616c3a063681123ff44bd0623a9858
Ludmila <pomohaibo@gmail.com>
2019-01-25 22:56:26 +0200

    Make getting all recipes sorted by date

--- backend/route/routes.js
+++ backend/route/routes.js
@@ -4,7 +4,7 @@ let Recipe = require('../model/recipe');
 
 router.route('/recipes')
     .get((req, res) => {
-        Recipe.find((err, items) => {
+        Recipe.find().sort({'cur_version.date': "desc"}).then((items, err) => {
             if (err) {
                 res.status(404).json(err);
             } else {
@@ -15,9 +15,13 @@ router.route('/recipes')
 router.route('/editRecipe')
     .put((req, res) => {
         let query = {_id: req.body._id};
-        Recipe.findOneAndUpdate(query, req.body, {new: true}, (err, doc) => {
-            if (err) return res.status(500).send(err);
-            return res.send(doc);
+        Recipe.findOneAndUpdate(query, req.body, {new: true}).then((doc, err) => {
+            if (!err) {
+                res.send(doc);
+            }
+            else {
+                res.status(500).send(err);
+            }
         });
     });
 
@@ -27,7 +31,7 @@ router.route('/newRecipe')
         newRecipe.save(function (err, doc) {
             if (err) return res.status(500).send(err);
             // Made this for getting object id that was not returned on post
-            Recipe.findOne({title:doc.title},(err,rescipe)=>{
+            Recipe.findOne({title: doc.title}, (err, rescipe) => {
                 if (err) return res.status(500).send(err);
                 return res.send(rescipe);
 
